ba6c5b1cc3b65eac86cf8fa43a1b2205bc8f0356
David Guillen Fandos <david@davidgf.net>
2015-06-30 20:06:35 +0100

    Ops!

--- xml.py
+++ xml.py
@@ -1,6 +1,9 @@
 
+def st(s):
+	return s.replace('"',"&quot;")
+
 def XN(name, attrs, data = []):
-	eattrs = " ".join([ '%s="%s"' % (k, attrs[k]) for k in attrs if attrs[k] ])
+	eattrs = " ".join([ '%s="%s"' % (st(k), st(attrs[k])) for k in attrs if attrs[k] ])
 	edata = "\n".join(data)
 
 	if len(data) == 0:
@@ -10,7 +13,7 @@ def XN(name, attrs, data = []):
 
 def XML(d):
 	d = "\n".join(d)
-	d = d.replace("&","&amp;").replace('"',"&quot;")
+	d = d.replace("&","&amp;")
 	return '<?xml version="1.0" encoding="UTF-8"?>\n' \
 	'<subsonic-response status="ok" version="1.4.0">\n' + d + '\n</subsonic-response>'
 
